ccaa1ddce2f20e03cbdaf7b1f594b8f7f91409e1
Cdec <junjun13m@gmail.com>
2017-12-23 13:05:55 +0900

    add dxlibrary

--- ports/dxlib/portfile.cmake
+++ ports/dxlib/portfile.cmake
@@ -10,24 +10,35 @@
 #   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
 #
 
+# Currently, only support "x86-windows-static"
+if(NOT ${TARGET_TRIPLET} MATCHES "x86-windows-static")
+        message(FATAL_ERROR "A supported triplet is only x86-windows-static.")
+endif()
+
+# Specify version
+set(VERSION "3.19")
+
+# Prepair sources
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/dxlib-3.19)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/DxLibMake)
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://dxlib.o.oo7.jp/DxLib/DxLibMake3_19.zip"
-    FILENAME "dxlib-3.19.zip"
+    URLS "http://dxlib.o.oo7.jp/DxLib/DxLibMake${VERSION}.zip"
+    FILENAME "dxlib-${VERSION}.zip"
     SHA512 4f55c65097a7aabc5a934fe0f306dd3d205638a38e0332ffb869fe3ab9355f45345e3756150962849b483725077ae3971188a901e2bd21d27f5be7c449eb7d81
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
+# Build
+vcpkg_build_msbuild(
+    PROJECT_PATH ${SOURCE_PATH}/DxLibMake.sln
+    PLATFORM "Win32"
 )
 
-vcpkg_install_cmake()
+# Copy libraries
+file(GLOB INDLUDES ${SOURCE_PATH}/*.h DESTINATION)
+file(INSTALL ${INDLUDES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL ${SOURCE_PATH}/Release/DxLib.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+file(INSTALL ${SOURCE_PATH}/Debug/DxLib_d.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
 
 # Handle copyright
-# file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/dxlib RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/DxLib.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/dxlib RENAME copyright)
