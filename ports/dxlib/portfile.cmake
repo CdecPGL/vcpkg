d78d9f98ad4f0ebf6627456a9fc8b7c2cfa75948
Cdec <junjun13m@gmail.com>
2017-12-24 02:49:42 +0900

    fix dxlib but not good

--- ports/dxlib/portfile.cmake
+++ ports/dxlib/portfile.cmake
@@ -17,17 +17,47 @@ vcpkg_download_distfile(ARCHIVE
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
-# Build
+# Build DXLibrary
 vcpkg_build_msbuild(
     PROJECT_PATH ${SOURCE_PATH}/DxLibMake.sln
     PLATFORM "Win32"
 )
 
-# Copy libraries
+# Copy DXLibrary's libraries
+file(INSTALL ${SOURCE_PATH}/Release/DxLib.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib RENAME DxLib_vs2015_x86.lib)
+file(INSTALL ${SOURCE_PATH}/Debug/DxLib_d.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib RENAME DxLib_vs2015_x86_d.lib)
+
+# Upgrade the solusion to build DirectX draw functions because the format of it is vcproj, which are not available for current MSBuild
+message(STATUS "Downloading vswhere.exe")
+file(DOWNLOAD https://github.com/Microsoft/vswhere/releases/download/2.2.11/vswhere.exe ${SOURCE_PATH}/vswhere.exe)
+execute_process(
+  COMMAND ./vswhere.exe -latest -property productPath -format value
+  WORKING_DIRECTORY ${SOURCE_PATH}
+  OUTPUT_VARIABLE DEVENV_PATH
+)
+string(STRIP ${DEVENV_PATH} DEVENV_PATH)
+message(STATUS "Upgrading DxDrawFuncMake.sln")
+vcpkg_execute_required_process(
+    COMMAND ${DEVENV_PATH} DxDrawFuncMake.sln /upgrade
+    WORKING_DIRECTORY ${SOURCE_PATH}
+    LOGNAME build-${TARGET_TRIPLET}-upgrade-solusion
+)
+
+# Build DirectX draw functions
+vcpkg_build_msbuild(
+    PROJECT_PATH ${SOURCE_PATH}/DxDrawFuncMake.sln
+    PLATFORM "Win32"
+)
+
+# Copy DirectX draw functions's libraries
+file(INSTALL ${SOURCE_PATH}/DrawFunc_Release/DxDrawFunc.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib RENAME DxDrawFunc_vs2015_x86.lib)
+file(INSTALL ${SOURCE_PATH}/DrawFunc_Debug/DxDrawFunc_d.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib RENAME DxDrawFunc_vs2015_x86_d.lib)
+
+# Copy includes
 file(GLOB INDLUDES ${SOURCE_PATH}/*.h)
 file(INSTALL ${INDLUDES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(INSTALL ${SOURCE_PATH}/Release/DxLib.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(INSTALL ${SOURCE_PATH}/Debug/DxLib_d.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
 
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/DxLib.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/dxlib RENAME copyright)
+
+vcpkg_copy_pdbs()
