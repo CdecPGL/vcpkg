39d5bf371d788dd84fc6011e23da96feb7d6a6b0
Cdec <junjun13m@gmail.com>
2017-12-23 14:31:48 +0900

    add effekseer

--- ports/effekseer/portfile.cmake
+++ ports/effekseer/portfile.cmake
@@ -17,29 +17,23 @@ if(NOT ${TARGET_TRIPLET} MATCHES "x86-windows-static")
         message(FATAL_ERROR "A supported triplet is only x86-windows-static.")
 endif()
 
-# Specify version
-set(VERSION "3.19")
-
 # Prepair sources
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/DxLibMake)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/EffekseerRuntime130)
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://dxlib.o.oo7.jp/DxLib/DxLibMake${VERSION}.zip"
-    FILENAME "dxlib-${VERSION}.zip"
-    SHA512 4f55c65097a7aabc5a934fe0f306dd3d205638a38e0332ffb869fe3ab9355f45345e3756150962849b483725077ae3971188a901e2bd21d27f5be7c449eb7d81
+    URLS "http://effekseer.sakura.ne.jp/Releases/EffekseerRuntime130.zip"
+    FILENAME "effekseer-1.30.zip"
+    SHA512 9708f7a1147e3be0d64be4114ece68a6af32d377ca8a95072472736f4da0342abfc3ec0a8172a7495c4038fb435d3706fa7c1a4c9a1200ca622c4454bb9335ae
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
-# Build
-vcpkg_build_msbuild(
-    PROJECT_PATH ${SOURCE_PATH}/DxLibMake.sln
-    PLATFORM "Win32"
-)
-
 # Copy libraries
-file(GLOB INDLUDES ${SOURCE_PATH}/*.h)
+file(GLOB INDLUDES ${SOURCE_PATH}/Compiled/include/*.h)
 file(INSTALL ${INDLUDES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(INSTALL ${SOURCE_PATH}/Release/DxLib.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(INSTALL ${SOURCE_PATH}/Debug/DxLib_d.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+file(GLOB REL_LIBS ${SOURCE_PATH}/Compiled/lib/VS2015/Release/*.lib)
+file(INSTALL ${REL_LIBS} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+file(GLOB DEB_LIBS ${SOURCE_PATH}/Compiled/lib/VS2015/Debug/*.lib)
+file(INSTALL ${DEB_LIBS} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/DxLib.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/dxlib RENAME copyright)
+file(DOWNLOAD https://github.com/effekseer/Effekseer/blob/master/LICENSE ${SOURCE_PATH}/LICENSE)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/effekseer RENAME copyright)
