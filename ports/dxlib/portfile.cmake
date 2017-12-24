397de4dc457f9268aff389c78d04da8f23c5656c
Cdec <junjun13m@gmail.com>
2017-12-24 12:12:21 +0900

    modify port for dxlib

--- ports/dxlib/portfile.cmake
+++ ports/dxlib/portfile.cmake
@@ -1,63 +1,35 @@
 # Specify version
-set(VERSION "3.19")
+set(VERSION 3.19)
 
 include(vcpkg_common_functions)
 
-# Currently, only support "x86-windows-static"
-if(NOT ${TARGET_TRIPLET} MATCHES "x86-windows-static")
-        message(FATAL_ERROR "A supported triplet is only x86-windows-static.")
+# Currently, only support "x[x86|64]-windows-static"
+if((NOT ${TARGET_TRIPLET} MATCHES "x86-windows-static") AND (NOT ${TARGET_TRIPLET} MATCHES "x64-windows-static"))
+        message(FATAL_ERROR "A supported triplet is only x86-windows-static and x64-windows-static.")
 endif()
 
 # Prepair sources
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/DxLibMake)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/DxLib_VC)
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://dxlib.o.oo7.jp/DxLib/DxLibMake${VERSION}.zip"
-    FILENAME "dxlib-${VERSION}.zip"
-    SHA512 4f55c65097a7aabc5a934fe0f306dd3d205638a38e0332ffb869fe3ab9355f45345e3756150962849b483725077ae3971188a901e2bd21d27f5be7c449eb7d81
+    URLS "http://dxlib.o.oo7.jp/DxLib/DxLib_${VERSION}.exe"
+    FILENAME "dxlib-${VERSION}.exe"
+    SHA512 36287fa941ffe15777ca4f8547aa933a0597051612739f07f8ffdf05a6c9b7c391d55b06774482596618f738b9e1e77eb132f8b0397000e165c64e806fd2b048
 )
-vcpkg_extract_source_archive(${ARCHIVE})
-
-# Build DXLibrary
-vcpkg_build_msbuild(
-    PROJECT_PATH ${SOURCE_PATH}/DxLibMake.sln
-    PLATFORM "Win32"
-)
-
-# Copy DXLibrary's libraries
-file(INSTALL ${SOURCE_PATH}/Release/DxLib.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib RENAME DxLib_vs2015_x86.lib)
-file(INSTALL ${SOURCE_PATH}/Debug/DxLib_d.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib RENAME DxLib_vs2015_x86_d.lib)
-
-# Upgrade the solusion to build DirectX draw functions because the format of it is vcproj, which are not available for current MSBuild
-message(STATUS "Downloading vswhere.exe")
-file(DOWNLOAD https://github.com/Microsoft/vswhere/releases/download/2.2.11/vswhere.exe ${SOURCE_PATH}/vswhere.exe)
-execute_process(
-  COMMAND ./vswhere.exe -latest -property productPath -format value
-  WORKING_DIRECTORY ${SOURCE_PATH}
-  OUTPUT_VARIABLE DEVENV_PATH
-)
-string(STRIP ${DEVENV_PATH} DEVENV_PATH)
-message(STATUS "Upgrading DxDrawFuncMake.sln")
+file(COPY ${ARCHIVE} DESTINATION ${CURRENT_BUILDTREES_DIR}/src)
+file(REMOVE_RECURSE ${SOURCE_PATH})
 vcpkg_execute_required_process(
-    COMMAND ${DEVENV_PATH} DxDrawFuncMake.sln /upgrade
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME build-${TARGET_TRIPLET}-upgrade-solusion
+    COMMAND ./dxlib-${VERSION}.exe
+    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/src
+    LOGNAME extract-${TARGET_TRIPLET}
 )
 
-# Build DirectX draw functions
-vcpkg_build_msbuild(
-    PROJECT_PATH ${SOURCE_PATH}/DxDrawFuncMake.sln
-    PLATFORM "Win32"
-)
-
-# Copy DirectX draw functions's libraries
-file(INSTALL ${SOURCE_PATH}/DrawFunc_Release/DxDrawFunc.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib RENAME DxDrawFunc_vs2015_x86.lib)
-file(INSTALL ${SOURCE_PATH}/DrawFunc_Debug/DxDrawFunc_d.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib RENAME DxDrawFunc_vs2015_x86_d.lib)
-
-# Copy includes
-file(GLOB INDLUDES ${SOURCE_PATH}/*.h)
+# Copy libraries
+file(GLOB INDLUDES ${SOURCE_PATH}/プロジェクトに追加すべきファイル_VC用/*.h)
 file(INSTALL ${INDLUDES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(GLOB REL_LIBS ${SOURCE_PATH}/プロジェクトに追加すべきファイル_VC用/*_vs2015_${TRIPLET_SYSTEM_ARCH}.lib)
+file(INSTALL ${REL_LIBS} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+file(GLOB DEB_LIBS ${SOURCE_PATH}/プロジェクトに追加すべきファイル_VC用/*_vs2015_${TRIPLET_SYSTEM_ARCH}_d.lib)
+file(INSTALL ${DEB_LIBS} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
 
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/DxLib.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/dxlib RENAME copyright)
-
-vcpkg_copy_pdbs()
